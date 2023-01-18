65dce533880faabecb0af109d08398bf50498ef7
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2023-01-17 22:35:48 -0300

    :sparkles: Adiciona o componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { Habit } from "./components/Habit"
+import { Header } from "./components/Header";
 import './styles/global.css';
 
 function App() {
   
 
   return (
-    <div className="bg-red-300 border">
-      <Habit completed={5} />
-      <Habit completed={4}/>
-      <Habit completed={3}/>
-      <Habit completed={5}/>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
     </div>
   )
 }
 
-export default App
+export { App }
